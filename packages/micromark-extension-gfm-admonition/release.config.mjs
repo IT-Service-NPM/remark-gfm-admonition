b2eb292f3996bff4a68fb7a58e1ae3796229d868
sergey-s-betke <sergey.s.betke@yandex.ru>
2026-03-02 13:21:50 +0300

    ci: disable using npm for publishing

--- packages/micromark-extension-gfm-admonition/release.config.mjs
+++ packages/micromark-extension-gfm-admonition/release.config.mjs
@@ -51,6 +51,9 @@ export default {
 All notable changes to this project will be documented in this file. See
 [Conventional Commits](https://conventionalcommits.org) for commit guidelines.`
     }],
+    ["@semantic-release/npm", {
+      npmPublish: false
+    }],
     ["@anolilab/semantic-release-pnpm"],
     "@semantic-release/github",
     ["@semantic-release/git", {
